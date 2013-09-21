01f3d4eefd6bcf2987ac24f7d5d147f400435616
cclim <austincclim@gmail.com>
2013-09-21 17:04:18 +0800

    route config

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
 	#Returns the full title on a per-page basis.
 	def full_title(page_title) #() to save argument from full_title of html and write in descriptive name, if no () def full_title is nothing.
-		base_title = "Ruby on Rails Tutorials Sample App "
+		base_title = "Ruby on Rails Tutorial Sample App "
 
 		if page_title.empty?
 			base_title
