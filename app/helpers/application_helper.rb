0f3750ea16f0194b50653bd63a2b290cc4bf37c4
cclim <austincclim@gmail.com>
2013-09-21 15:05:40 +0800

    helper refactor

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+
+	#Returns the full title on a per-page basis.
+	def full_title(page_title) #() to save argument from full_title of html and write in descriptive name, if no () def full_title is nothing.
+		base_title = "Ruby on Rails Tutorials Sample App "
+
+		if page_title.empty?
+			base_title
+		else
+			"#{base_title} | #{page_title}"
+		end
+	end
+
 end
